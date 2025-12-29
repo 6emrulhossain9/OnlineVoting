ced32429155ce22eda66a1217c5b46596686843e
Emrul <emrulhossain49@gmail.com>
2025-12-29 21:33:04 +0600

    Update candidates and users table structure
    Added email and photo fields to candidates and users tables.

--- online_voting.sql
+++ online_voting.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 11, 2025 at 04:13 PM
+-- Generation Time: Dec 29, 2025 at 03:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,23 +30,25 @@ SET time_zone = "+00:00";
 CREATE TABLE `candidates` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `position` varchar(100) NOT NULL,
   `event_id` int(11) NOT NULL,
-  `votes` int(11) NOT NULL DEFAULT 0
+  `votes` int(11) NOT NULL DEFAULT 0,
+  `photo` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `candidates`
 --
 
-INSERT INTO `candidates` (`id`, `name`, `position`, `event_id`, `votes`) VALUES
-(5, 'Alhaz', 'CR', 1, 0),
-(6, 'Siam', 'GS', 3, 0),
-(8, 'Rakib', 'CR', 1, 0),
-(13, 'Emrul', 'President', 2, 0),
-(14, 'Minhaz', 'President', 2, 0),
-(15, 'Sifat', 'GS', 3, 0),
-(17, 'Mmm', '', 0, 0);
+INSERT INTO `candidates` (`id`, `name`, `email`, `position`, `event_id`, `votes`, `photo`) VALUES
+(5, 'Alhaz', NULL, 'CR', 1, 0, NULL),
+(6, 'Siam', NULL, 'GS', 3, 0, NULL),
+(8, 'Rakib', NULL, 'CR', 1, 0, NULL),
+(13, 'Emrul', NULL, 'President', 2, 0, NULL),
+(14, 'Minhaz', NULL, 'President', 2, 0, NULL),
+(15, 'Sifat', NULL, 'GS', 3, 0, NULL),
+(17, 'Mmm', NULL, '', 0, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -80,6 +82,7 @@ INSERT INTO `events` (`id`, `title`, `start_time`, `end_time`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
   `full_name` varchar(100) NOT NULL,
   `student_id` varchar(50) NOT NULL,
   `phone_number` varchar(20) NOT NULL,
@@ -93,14 +96,15 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `full_name`, `student_id`, `phone_number`, `department`, `section`, `password`, `role`) VALUES
-(4, 'admin', '', '', '', '', '', '$2y$10$idw4VkfJY5vVZzwxHxeWDuoEG1ZabIg6aCTmcqpbkfPwULCOMOB4C', 'admin'),
-(7, 'nothing', 'emrul2', '412303016099', '01753990690', 'CSE', '6F', '$2y$10$LjOhY4vv261UtIWPCajVJ.DoW6D.RYjk3C5YhIBH.M16RSlTw7eaW', 'voter'),
-(8, 'rakib110', 'Rakib', '12', '01234567891', 'CSE', 'F', '$2y$10$KNIZJBMeXJGw9CvC4DJn0OSer5jIIMAeUt0spv3/lN4gjlfFs7AOe', 'voter'),
-(9, 'minu', 'minhazul islam', '41230301611', '01712345678', 'CSE', '6F', '$2y$10$.wiVqKrayrs6/S7BUSeR5O/XebwRHxKBqQAGkyA7/dBsPi71rhFKe', 'voter'),
-(11, 'no', '', '', '', '', '', '$2y$10$Y.PMyETTqhhztVLTMmMQp.85i08FxLK8L5NE8YY2DC1hVuBgBk5sG', 'voter'),
-(12, 'hello', '', '', '', '', '', '$2y$10$moPtQOz/LxHASw8tGL50feL9b2zCFmb3zfLXM4LXXVZe9M5l8sgZC', 'voter'),
-(13, 'MINHAZ', '', '', '', '', '', '$2y$10$Yvoco7vMWi//VKwVTIdoh.yh6aSM6s3uTsbY77eNjLsdl1RyKyn2.', 'voter');
+INSERT INTO `users` (`id`, `username`, `email`, `full_name`, `student_id`, `phone_number`, `department`, `section`, `password`, `role`) VALUES
+(4, 'admin', NULL, '', '', '', '', '', '$2y$10$idw4VkfJY5vVZzwxHxeWDuoEG1ZabIg6aCTmcqpbkfPwULCOMOB4C', 'admin'),
+(7, 'nothing', NULL, 'emrul2', '412303016099', '01753990690', 'CSE', '6F', '$2y$10$LjOhY4vv261UtIWPCajVJ.DoW6D.RYjk3C5YhIBH.M16RSlTw7eaW', 'voter'),
+(8, 'rakib110', NULL, 'Rakib', '12', '01234567891', 'CSE', 'F', '$2y$10$KNIZJBMeXJGw9CvC4DJn0OSer5jIIMAeUt0spv3/lN4gjlfFs7AOe', 'voter'),
+(9, 'minu', NULL, 'minhazul islam', '41230301611', '01712345678', 'CSE', '6F', '$2y$10$.wiVqKrayrs6/S7BUSeR5O/XebwRHxKBqQAGkyA7/dBsPi71rhFKe', 'voter'),
+(11, 'no', NULL, '', '', '', '', '', '$2y$10$Y.PMyETTqhhztVLTMmMQp.85i08FxLK8L5NE8YY2DC1hVuBgBk5sG', 'voter'),
+(12, 'hello', NULL, '', '', '', '', '', '$2y$10$moPtQOz/LxHASw8tGL50feL9b2zCFmb3zfLXM4LXXVZe9M5l8sgZC', 'voter'),
+(13, 'MINHAZ', NULL, '', '', '', '', '', '$2y$10$Yvoco7vMWi//VKwVTIdoh.yh6aSM6s3uTsbY77eNjLsdl1RyKyn2.', 'voter'),
+(14, 'siaaam', 'siamparvez@gmail.com', 'siam', '41230301609', '01706176979', 'Computer Science', '1F', '$2y$10$thvQe16oByvypm.hUD.7OeKUwt.hZfOa0GkLWC5GtVpr6tbK9Us6e', 'voter');
 
 -- --------------------------------------------------------
 
@@ -166,7 +170,7 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `votes`
